a996bbba36e02207f958517e6f7136d3baf5cc1b
Blake Eriks <eblake98@vt.edu>
2022-02-08 18:00:11 -0800

    skeleton nav bar

---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+import { Flex, Text } from "@chakra-ui/react"
+
+const Navbar = () => {
+  return (
+    <Flex w="100%" bg="gray.700" color="gray.50">
+      <Text fontSize="4xl">
+        🛸 Eye In The Sky
+      </Text>
+    </Flex>
+  )
+}
+
+export default Navbar
