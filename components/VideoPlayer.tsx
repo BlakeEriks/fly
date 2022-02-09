a41295ffdf6dc932f6880367711f020a6c387ace
Blake Eriks <eblake98@vt.edu>
2022-02-08 18:00:41 -0800

    Skeleton components

---
+++ components/VideoPlayer.tsx
@@ -0,0 +1,11 @@
+import { Flex } from "@chakra-ui/react"
+
+const VideoPlayer = () => {
+  return (
+    <Flex flexGrow={1}>
+      Video Player
+    </Flex>
+  )
+}
+
+export default VideoPlayer
---
+++ components/VideoSelector.tsx
@@ -0,0 +1,11 @@
+import { Flex } from "@chakra-ui/react"
+
+const VideoSelector = () => {
+  return (
+    <Flex>
+      Video Selector
+    </Flex>
+  )
+}
+
+export default VideoSelector
