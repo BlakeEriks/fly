acd9a518c9abb16b150fbd14ba3c917f460710f0
Blake Eriks <eblake98@vt.edu>
2022-02-12 15:07:22 -0800

    Add temp video

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -1,9 +1,23 @@
 import { Flex } from "@chakra-ui/react"
 
-const VideoPlayer = () => {
+interface VideoPlayerProps {
+  videoId: string
+}
+
+const VideoPlayer = (props: VideoPlayerProps) => {
+
+  console.log(props.videoId)
+
   return (
     <Flex flexGrow={1}>
       Video Player
+      <video autoPlay={true} controls width="1000">
+
+        <source 
+        src={`https://storage.cloud.google.com/video_drone/Lulumahu_Falls/0345.MP4`}
+            type="video/webm">
+        </source>
+      </video>
     </Flex>
   )
 }
