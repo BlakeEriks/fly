3476137b5a05930aa92f4fdb8e67ceb3fdab6fe6
Blake Eriks <eblake98@vt.edu>
2022-02-23 17:50:51 -0800

    Hide temporarily

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,9 +2,9 @@ import { Flex, Text } from "@chakra-ui/react"
 
 const Navbar = () => {
   return (
-    <Flex w="100%" bg="gray.700" color="gray.50">
-      <Text fontSize="4xl">
-        🛸 Eye In The Sky
+    <Flex w="100%" justify="center" bg="gray.700" color="gray.50">
+      <Text fontSize={30} my={0}>
+        {/* 🛸 Eye In The Sky */}
       </Text>
     </Flex>
   )
