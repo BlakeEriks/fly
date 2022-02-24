e72f518e127282f8836c7684c4724cbd3ea29234
Blake Eriks <eblake98@vt.edu>
2022-02-23 17:51:24 -0800

    Create Video Selector

--- components/VideoSelector.tsx
+++ components/VideoSelector.tsx
@@ -1,16 +1,31 @@
-import { Flex } from "@chakra-ui/react"
+import { Box, Button, Flex } from "@chakra-ui/react"
+import { Location } from "utils/types"
 
 interface VideoSelectorProps {
-  videos: any
   setSelectedVideo: Function
+  videoIds: string[]
+  location: Location
 }
 
-const VideoSelector = ({setSelectedVideo, videos}: VideoSelectorProps) => {
+const VideoSelector = ({setSelectedVideo, videoIds, location}: VideoSelectorProps) => {
+
+  const getVideoUrl = (videoId: string) => location && videoId ? `https://storage.cloud.google.com/video_drone/${location.name}/${videoId}.mp4` : undefined
+  console.log(videoIds)
 
   return (
-    <Flex>
-      Video Selector
-      {videos.map( (video: any, index: number) => <div key={index}>video {index}</div>)}
+    <Flex flexWrap="wrap" justify="space-between" alignItems="center" w="60%" boxShadow="0px 0px 5px rgba(0,0,0,1) inset" p={4} rounded={16} ml={8}>
+      {videoIds?.map( (videoId: string) => (
+        <Box 
+          key={videoId}
+          w="30%"
+        >
+          <button onClick={() => setSelectedVideo(videoId)}>
+            <video width="100%" preload="metadata">
+              <source src={`${getVideoUrl(videoId)}#t=0.5`} type="video/mp4" />
+            </video>
+          </button>
+        </Box>
+      ))}
     </Flex>
   )
 }
