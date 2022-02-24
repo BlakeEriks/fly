49fc942b55c49d7f84d4b32bb51c7db1e3b2f986
Blake Eriks <eblake98@vt.edu>
2022-02-23 17:50:45 -0800

    Add location text

--- components/LocationSelector.tsx
+++ components/LocationSelector.tsx
@@ -1,31 +1,38 @@
-import { Flex } from "@chakra-ui/react"
+import { Flex, Text } from "@chakra-ui/react"
+import { useState } from "react";
 import Map, { Marker } from 'react-map-gl';
-import { Map as MyMap }  from "../utils/types";
+import { Location, Map as MyMap }  from "../utils/types";
 import Pin from "./Pin";
 
 interface LocationSelectorProps {
-  map: MyMap | null
+  map: MyMap
+  selectedLocation?: Location
   setSelectedLocation: Function
 }
 
-const LocationSelector = ({ map, setSelectedLocation }: LocationSelectorProps) => {
+const LocationSelector = ({ map, selectedLocation, setSelectedLocation }: LocationSelectorProps) => {
+
+  const [locationText, setLocationText] = useState(selectedLocation)
+  const [hoverLocation, setHoverLocation] = useState('')
 
   return (
-    <Flex w="33%" direction="column" position="relative">
-      Location Selector
+    <Flex w="40%" direction="column" justify="center" align="center" position="relative" boxShadow="0px 0px 5px rgba(0,0,0,1) inset" p={4} rounded={16}>
       <Map
         mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
-        style={{width: 400, height: 300}}
+        style={{width: "100%"}}
         mapStyle="mapbox://styles/blakeeriks/ckzdcbisu001t14qc0qcyv2nc"
         attributionControl={false}
         {...map}
       >
-        {map?.locations.map(({longitude, latitude, name}, index) => (
-          <Marker key={index} longitude={longitude} latitude={latitude} anchor="bottom" >
-            <Pin onClick={() => setSelectedLocation(name)}/>
+        {map?.locations.map((location, index) => (
+          <Marker key={index} longitude={location.longitude} latitude={location.latitude} anchor="bottom">
+            <Pin onClick={() => setSelectedLocation(location)} selected={selectedLocation === location} onMouseEnter={() => setHoverLocation(location.name)} onMouseLeave={() => setHoverLocation('')}/>
           </Marker>
         ))}
       </Map>
+      <Text position="absolute" bottom={4} left={4}>
+          {hoverLocation ? hoverLocation : selectedLocation?.name}
+      </Text>
     </Flex>
   )
 }
