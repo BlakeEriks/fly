d589007f6d975ee75db96c4f49c6f5ed4998cc0d
Blake Eriks <eblake98@vt.edu>
2022-02-23 17:51:31 -0800

    Add Chakra theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from "@chakra-ui/theme"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ChakraProvider theme={theme}>
       <Head>
         <link href="https://api.mapbox.com/mapbox-gl-js/v0.51.0/mapbox-gl.css" rel="stylesheet" />
       </Head>
       <Component {...pageProps} />
-    </>
+    </ChakraProvider>
   )
 }
 
