bb0df911304d653ac62f09a4f1dccde56471852a
Blake Eriks <eblake98@vt.edu>
2022-02-23 17:51:13 -0800

    Improve styles

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -1,23 +1,28 @@
 import { Flex } from "@chakra-ui/react"
+import { ReactEventHandler } from "react"
+import { Location } from "utils/types"
 
 interface VideoPlayerProps {
-  videoId: string
+  videoId: string | undefined
+  location: Location | undefined
+  nextVideo: ReactEventHandler
 }
 
-const VideoPlayer = (props: VideoPlayerProps) => {
+const VideoPlayer = ({location, videoId, nextVideo}: VideoPlayerProps) => {
 
-  console.log(props.videoId)
+  const videoUrl = location && videoId ? `https://storage.cloud.google.com/video_drone/${location.name}/${videoId}.mp4` : null
+
+  console.log(videoUrl)
 
   return (
-    <Flex flexGrow={1}>
-      Video Player
-      <video autoPlay={true} controls width="1000">
-
+    <Flex flexGrow={1} h="60vh" justify="center" boxShadow="0px 0px 5px rgba(0,0,0,1) inset" p={4} rounded={16}>
+      {videoUrl &&
+      <video autoPlay={true} width="100%" height="100%" key={videoUrl} onEnded={nextVideo} style={{background: "black"}}>
         <source 
-        src={`https://storage.cloud.google.com/video_drone/Lulumahu_Falls/0345.MP4`}
-            type="video/webm">
+          src={videoUrl}
+          type="video/webm">
         </source>
-      </video>
+      </video>}
     </Flex>
   )
 }
